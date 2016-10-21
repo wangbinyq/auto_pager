18333014be2434b81f5edb6d4163e99e388ca8ab
wangbin <wangbin_ydkf@kedacom.com>
2016-10-21 15:48:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         autopager
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  auto pager
+// @author       You
+// @match        http://tampermonkey.net/scripts.php
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    console.log('test UserScript');
+})();
