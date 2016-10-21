0bc139f48a04a213059b33c9c6e4564805139bcb
wangbin <wangbin_ydkf@kedacom.com>
2016-10-21 15:40:08 +0800

    init project

---
+++ test/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "env": {
+    "jasmine": true
+  },
+  "globals": {
+    "$": true,
+    "jQuery": true,
+    "casper": true,
+    "getWarnCount": true
+  },
+  "rules": {
+    "no-multi-str": 0,
+    "object-curly-spacing": 0,
+    "array-bracket-spacing": 0,
+    "no-unused-vars": 0
+  }
+}
