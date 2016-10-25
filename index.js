7b39e251fc1cd65475547c56439e566e9b57f796
wangbin <wangbin_ydkf@kedacom.com>
2016-10-25 19:50:10 +0800

    page template ok

--- index.js
+++ index.js
@@ -258,13 +258,154 @@
     fn.init = function() {
     
         var style = document.createElement('style')
-        style.innerText = `
+        style.innerHTML = `
             #_autopager {
-                position: absolute
-                right: 5px
-                bottom: 5px
+                position: absolute;
+                right: 5px;
+                bottom: 5px;
+                box-shadow: 0 0 5px #aaaaaa;
+                padding: 20px;
+                width: 200px;
+                font-size: 14px;
+                font-family: "Roboto", "Noto Sans CJK SC", "Nato Sans CJK TC", "Nato Sans CJK JP", "Nato Sans CJK KR", -apple-system, ".SFNSText-Regular", "Helvetica Neue", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Zen Hei", Arial, sans-serif;
             }
 
+            #_autopager input[type="text"] {
+                width: 70%;
+                padding: 5px 9px;
+                border: 1px solid hsl(0, 0%, 70%);
+                margin: 0px;
+                text-align: left;
+                outline: none;
+            }
+
+            #_autopager input[type="text"]:hover {
+                border: 1px solid hsl(0, 0%, 50%);
+            }
+
+            #_autopager input[type="text"]:focus {
+                border: 1px solid hsl(200, 40%, 60%);
+            }
+
+            #_autopager .flex {
+                display: flex;
+                align-items: center;
+                margin: 10px 0;
+            }
+
+            #_autopager .flex-grow {
+                flex-grow: 999;
+            }
+
+            #_autopager .line {
+                width: 100%;
+                height: 1px;
+                background: hsl(0, 0%, 80%);
+                margin: 20px 0;
+            }
+
+            #_autopager #close {
+                display: none;
+            }
+            
+            #_autopager:hover #close {
+                display: block;
+                position: absolute;
+                right: 6px;
+                top: 6px;
+                width: 12px;
+                height: 12px;
+            }
+
+            #_autopager:hover #close:hover {
+                height: 14px;
+            }
+
+            #_autopager:hover #close::before,
+            #_autopager:hover #close::after {
+                content: "";
+                display: block;
+                width: 2px;
+                height: 100%;
+                background: hsl(0, 0%, 20%);;
+                position: absolute;
+                left: 6px;
+            }        
+
+            #_autopager:hover #close::before {
+                transform: rotate(45deg);
+            }
+
+            #_autopager:hover #close::after {
+                transform: rotate(-45deg);
+            }
+
+            #_autopager:hover #close:active::before,
+            #_autopager:hover #close:active::after {
+                background: hsl(0, 0%, 0%);;
+            }
+
+            #_autopager .btn {
+                font-size: 14px;
+                border-radius: 2px;
+                border: 0;
+                text-align: center;
+                background-color: hsl(200, 60%, 50%);
+                transition: all .2s ease-in-out;
+                color: white;
+                padding: 4px 8px;
+            }
+
+            #_autopager .btn:hover {
+                background-color: hsl(200, 70%, 45%);
+            }
+
+            #_autopager .btn:active {
+                background-color: hsl(200, 80%, 40%);
+            }
+
+            #_autopager .select {
+                position: relative;
+                left: -5px;
+                width: 10px;
+                height: 10px;
+                border: 1px solid hsl(0, 0%, 30%);
+                border-radius: 5px;
+            }
+
+            #_autopager .select:hover {
+                border-color: hsl(0, 0%, 20%);
+            }
+
+            #_autopager .select:active {
+                border-color: hsl(0, 0%, 0%);
+            }
+
+            #_autopager .select::before,
+            #_autopager .select::after {
+                content: "";
+                display: inline-block;
+                width: 2px;
+                height: 20px;
+                background: hsl(0, 0%, 30%);;
+                position: absolute;
+                left: 4px;
+                top: -5px;
+            }
+
+            #_autopager .select::after {
+                transform: rotate(90deg);
+            }
+
+            #_autopager .select:hover::before,
+            #_autopager .select:hover::after {
+                background: hsl(0, 0%, 20%);
+            }
+
+            #_autopager .select:active::before,
+            #_autopager .select:active::after {
+                background: hsl(0, 0%, 0%);
+            }
         `
         document.head.appendChild(style)
 
@@ -272,9 +413,29 @@
         el.id = '_autopager'
         el.dataset.show = 'selectMode'
         el.innerHTML = `
-            div.
-        `
+            <i id="close"></i>
+            <div class="flex">
+                <span>选择内容</span>
+                <span class="flex-grow"></span>
+                <button class="btn">添加</button>
+            </div>
+            
+            <div class="flex">
+                <input type="text"/>
+                <span class="flex-grow"></span>                
+                <span class="select"></span>
+            </div>
 
+            <div class="line"></div>
+
+            <span>选择下一页</span>
+            <div class="flex">
+                <input type="text"/>
+                <span class="flex-grow"></span>                
+                <span class="select"></span>
+            </div>
+        `
+        
         document.body.appendChild(el)
     }
 
