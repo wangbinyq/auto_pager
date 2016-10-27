330c91087c37323eeb49a6147e367d7d1380d60f
wangbin <wangbin_ydkf@kedacom.com>
2016-10-27 14:30:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
             loader: 'babel',
             query: {
                 presets: ['es2015'],
-                plugins: ["transform-vue-jsx"]
             }
         }, {
             test: /\.css/,
@@ -19,4 +18,21 @@ module.exports = {
         }]
     },
     devtool: 'source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
 }
